a3c0e24373de37686a2df335ea668d09389e1f6c
Evgeniy Zinchenko <dr.menson@gmail.com>
2023-09-03 15:11:17 +0300

    this is for lsp signature

--- lua/Evgeniy/plug.lua
+++ lua/Evgeniy/plug.lua
@@ -91,6 +91,7 @@ return packer.startup(function(use)
 			{ "rafamadriz/friendly-snippets" },
 		},
 	})
+	use("ray-x/lsp_signature.nvim")
 	use("jose-elias-alvarez/null-ls.nvim")
 	-- DAP and debuggers
 	use("mfussenegger/nvim-dap")
